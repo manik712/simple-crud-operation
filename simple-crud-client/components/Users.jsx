5bd87801ae2722f2fefcaa42725a19807c044444
manik712 <ranineela71@gmail.com>
2023-10-14 03:28:46 +0600

    added

--- simple-crud-client/components/Users.jsx
+++ simple-crud-client/components/Users.jsx
@@ -1,32 +1,45 @@
+import { useState, useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
-  const Users = useLoaderData();
-  console.log(Users);
+  const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
+
+  console.log(loadedUsers);
   const handleDelete = (_id) => {
-    console.log("delete", _id);
     fetch(`http://localhost:5000/users/${_id}`, {
-      method: 'DELETE',
+      method: "DELETE",
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        if(data.deletedCount>0){
-          alert("deleted")
+        if (data.deletedCount > 0) {
+          alert("Deleted");
+          const remainingUsers = users.filter((user) => user._id !== _id);
+          setUsers(remainingUsers);
         }
+      })
+      .catch((error) => {
+        console.error("Error deleting user:", error);
       });
   };
 
+  useEffect(() => {
+    setUsers(loadedUsers); // Update the users when new data is loaded
+  }, [loadedUsers]);
+
   return (
     <div>
-      {Users.length}
+      <p>Total Users: {users.length}</p>
       <div>
-        {Users.map((user) => (
-          <p key={user._id}>
-            {user.name}:{user.email}
-            {user._id}
-            <button onClick={() => handleDelete(user._id)}>delete</button>
-          </p>
+        {users.map((user) => (
+          <div key={user._id}>
+            <p>
+              Name: {user.name}<br />
+              Email: {user.email}<br />
+              ID: {user._id}
+            </p>
+            <button onClick={() => handleDelete(user._id)}>Delete</button>
+          </div>
         ))}
       </div>
     </div>
